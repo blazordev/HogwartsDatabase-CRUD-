d3e54668be7015105cf10956dd058d28a619cf7c
avi <avifriedman20@gmail.com>
2020-06-29 22:55:48 -0400

    Removed Radzen, Added single select roles

--- Hogwarts.Server/Pages/StaffListBase.cs
+++ Hogwarts.Server/Pages/StaffListBase.cs
@@ -1,4 +1,5 @@
-﻿using Hogwarts.Data.Models;
+﻿using Hogwarts.Data;
+using Hogwarts.Data.Models;
 using Hogwarts.Server.Services;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Http.Extensions;
@@ -13,32 +14,39 @@ namespace Hogwarts.Server.Pages
 {
     public class StaffListBase : ComponentBase
     {
-        public bool isSortedAscending;
-        public string activeSortColumn;
         [Inject] StaffDataService StaffDataService { get; set; }
         [Inject] RolesDataService RoleDataService { get; set; }
         [Inject] NavigationManager NavigationManager { get; set; }
+
+
+        public IEnumerable<RoleDto> Roles { get; set; } = new List<RoleDto>();
         public IEnumerable<StaffDto> Staff { get; set; } = new List<StaffDto>();
         [Parameter] public string SearchTerm { get; set; } = "";
-        public int SearchRole { get; set; }
-        public IEnumerable<RoleDto> Roles { get; set; } = new List<RoleDto>();
 
-        public List<StaffDto> FilteredStaff
+        private IEnumerable<StaffDto> _filteredStaff;
+        public IEnumerable<StaffDto> FilteredStaff
         {
-            get { return GetFilteredStaff().ToList(); }
-            set { FilteredStaff = value; }
+            get
+            { return PreformFilter(); }
+            set
+            { _filteredStaff = value; }
         }
-        public IEnumerable<StaffDto> GetFilteredStaff()
+
+        public IEnumerable<StaffDto> PreformFilter()
         {
-            Console.WriteLine("Filtered");
-            return Staff.Where(s =>
+            IEnumerable<StaffDto> list;
+            list = Staff.Where(s =>
             s.FirstName.ToLower().Contains(SearchTerm.ToLower())
             || s.MiddleNames.ToLower().Contains(SearchTerm.ToLower())
             || s.LastName.ToLower().Contains(SearchTerm.ToLower()));
-
+            if (selectedRole != 0)
+            {
+                list = list.Where(s => s.Roles.Any(r => r.Id == selectedRole));
+            }
             
-
+            return list;
         }
+
         protected override async Task OnInitializedAsync()
         {
             Staff = await StaffDataService.GetAllStaffAsync();
@@ -48,27 +56,15 @@ namespace Hogwarts.Server.Pages
         {
             NavigationManager.NavigateTo($"staffDetails/{staffId}");
         }
+        public int selectedRole { get; set; }
         public void RoleSelected(ChangeEventArgs e)
         {
-            System.Diagnostics.Debug.WriteLine("whatever");
-            //if (int.TryParse((string)e.Value, out var index) && index >= 0)
-            //{
-            //    List<StaffDto> List = new List<StaffDto>();
-            //    foreach (var staff in FilteredStaff)
-            //    {
-            //        foreach (var role in staff.Roles)
-            //        {
-            //            if (role.Id == index)
-            //            {
-            //                List.Add(staff);
-            //                break;
-            //            }
-            //        }
-            //    }
-            //    FilteredStaff = List;
-            //}
-
+            if (int.TryParse((string)e.Value, out var index))
+            {
+                selectedRole = index;
+            }
         }
     }
 }
+
 
