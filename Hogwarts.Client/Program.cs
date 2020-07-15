fbb08d4b3960c3575f17901a1dea8c539ba0cdc9
avi <avifriedman20@gmail.com>
2020-07-15 03:45:00 -0400

    added styling to table

--- Hogwarts.Client/Program.cs
+++ Hogwarts.Client/Program.cs
@@ -8,7 +8,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Hogwarts.Client.Services;
-using BlazorStrap;
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 
 namespace Hogwarts.Client
 {
@@ -18,13 +20,24 @@ namespace Hogwarts.Client
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
-            builder.Services.AddBootstrapCss();
+            builder.Services.AddBlazorise(options =>
+                {
+                    options.ChangeTextOnKeyPress = true;
+                })
+                .AddBootstrapProviders()
+                .AddFontAwesomeIcons();
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri("https://localhost:5001/") });
             builder.Services.AddScoped<StaffDataService>();
             builder.Services.AddScoped<StudentDataService>();
             builder.Services.AddScoped<RolesDataService>();
             builder.Services.AddScoped<HouseDataService>();
             builder.Services.AddScoped<CourseDataService>();
+            var host = builder.Build();
+
+            host.Services
+              .UseBootstrapProviders()
+              .UseFontAwesomeIcons();
+
             await builder.Build().RunAsync();
         }
     }
